e9710a40107b391a4da35df30f8669ec3aa7fcc5
Reha Sterbin <reha@omniti.com>
2016-12-07 15:19:01 -0500

    Added set-data functionality and test file

--- src/plugins/subquery/plugin.js
+++ src/plugins/subquery/plugin.js
@@ -4,6 +4,15 @@
  */
 
 Selectors.subquery_container = '.rules-subquery-container';
+QueryBuilder.types['subquery'] = 'subquery';
+QueryBuilder.OPERATORS['equal'].apply_to.push('subquery');
+QueryBuilder.OPERATORS['not_equal'].apply_to.push('subquery');
+QueryBuilder.OPERATORS['in'].apply_to.push('subquery');
+QueryBuilder.OPERATORS['not_in'].apply_to.push('subquery');
+QueryBuilder.OPERATORS['is_empty'].apply_to.push('subquery');
+QueryBuilder.OPERATORS['is_not_empty'].apply_to.push('subquery');
+QueryBuilder.OPERATORS['is_null'].apply_to.push('subquery');
+QueryBuilder.OPERATORS['is_not_null'].apply_to.push('subquery');
 
 QueryBuilder.define('subquery', function(options) {
     this.status.subquery_id = 0;
@@ -13,9 +22,11 @@ QueryBuilder.define('subquery', function(options) {
         var self = e.builder;
         self.filters.forEach(function(filter, i) {
             if ('subquery' in filter) {
+                self.filters[i].type = 'subquery';
                 self.filters[i].input = self.createSubquery;
                 self.filters[i].validation = { callback: self.validateSubquery };
                 self.filters[i].valueGetter = self.getSubqueryValue;
+                self.filters[i].valueSetter = self.setSubqueryValue;
             }
         }, self);
     });
@@ -24,6 +35,11 @@ QueryBuilder.define('subquery', function(options) {
     this.on('afterCreateRuleInput', function(e, rule) {
         var self = e.builder;
         self.initSubquery(rule);
+    });
+
+    // Drop the custom input function from json
+    this.on('ruleToJson.filter', function(e, rule) {
+        delete e.value.input;
     });
 
 });
@@ -74,7 +90,6 @@ QueryBuilder.extend({
      */
     validateSubquery: function(value, rule) {
         if (!('subquery_id' in rule)) {
-            console.log(rule);
             Utils.error('subquery', 'Subquery id missing in "{0}" on validateSubquery', rule.id);
         }
         var $b = $('#' + rule.subquery_id);
@@ -99,6 +114,20 @@ QueryBuilder.extend({
         var $b = $('#' + rule.subquery_id);
         opts = $.extend({ validate: false }, opts);
         return $b.queryBuilder('getRules', opts);
+    },
+
+    /**
+     * Sets the subquery value
+     * @param rule {Rule}
+     * @param value {object}
+     * @return {object}
+     */
+    setSubqueryValue: function(rule, value) {
+        if (!('subquery_id' in rule)) {
+            Utils.error('subquery', 'Subquery id missing in "{0}" on setSubqueryValue', rule.id);
+        }
+        var $b = $('#' + rule.subquery_id);
+        return $b.queryBuilder('setRules', value);
     }
 
 });
---
+++ tests/plugins.subquery.module.js
@@ -0,0 +1,104 @@
+$(function () {
+    var $b = $('#builder');
+
+    QUnit.module('plugins.subquery', {
+        afterEach: function () {
+            $b.queryBuilder('destroy');
+        }
+    });
+
+    QUnit.test('Get/set subquery rules', function (assert) {
+        $b.queryBuilder({
+            filters: subquery_filters,
+            rules: subquery_rules,
+            plugins: ['subquery']
+        });
+
+        var result = $b.queryBuilder('getRules');
+        assert.rulesMatch(
+            result.rules[0].value,
+            subquery_rules.rules[0].value,
+            'Should return the rules it set for the subquery builder'
+        );
+
+        // For some reason, leaving these in causes max-diff errors
+        delete result.rules[0].value;
+        delete subquery_rules.rules[0].value;
+        assert.rulesMatch(
+            result,
+            subquery_rules,
+            'Should return the rules it set for the main builder'
+        );
+    });
+
+    QUnit.test('Validate subquery rules', function (assert) {
+        $b.queryBuilder({
+            filters: subquery_filters,
+            plugins: ['subquery']
+        });
+
+        $('[name=builder_rule_0_filter]').val('subscriptions').trigger('change');
+        $('[name=builder_subquery_0_rule_0_filter]').val('name').trigger('change');
+        $b.queryBuilder('validate');
+
+        assert.equal(
+            $b.find('.error-container').eq(1).attr('title'),
+            'Error in subquery',
+            'Should indicate an error in the subquery'
+        );
+
+        assert.equal(
+            $b.find('.error-container').eq(3).attr('title'),
+            'Empty value',
+            'Should note missing value in first subquery rule'
+        );
+    });
+
+    var subquery_filters = [{
+        id: "subscriptions",
+        label: "Subscriptions",
+        subquery: {
+            default_filter: 'name',
+            display_empty_filter: false,
+            filters: [{
+                id: "name",
+                type: "string",
+                label: "Name"
+            }, {
+                id: "status",
+                type: "string",
+                label: "Status",
+                input: "select",
+                multiple: true,
+                values: {
+                    ac: "Active",
+                    in: "Inactive",
+                    tr: "Terminated"
+                }
+            }]
+        }
+    }];
+
+    var subquery_rules = {
+        condition: 'AND',
+        rules: [{
+            id: 'subscriptions',
+            field: 'subscriptions',
+            type: 'subquery',
+            operator: 'in',
+            value: {
+                condition: 'AND',
+                rules: [{
+                    id: 'name',
+                    operator: 'contains',
+                    value: 'Hello'
+                }, {
+                    id: 'status',
+                    operator: 'equal',
+                    value: 'ac'
+                }]
+            }
+        }]
+    };
+
+});
